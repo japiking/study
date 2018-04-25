3aab57484ded70c27da1f43aeadc35f266565510
japiking <37947897+japiking@users.noreply.github.com>
2018-04-25 13:15:38 +0900

    aaa

--- src/study/java/enm/MainEnum2.java
+++ src/study/java/enm/MainEnum2.java
@@ -2,7 +2,17 @@ package study.java.enm;
 
 public class MainEnum2 {
 	public static void main(String[]args) {
-		Calculater calc = Calculater.CALC_A;
-		calc.calculater(10);
+		
+		Calculater calc = Calculater.CALC_B;
+		long tmp = calc.calculater(10);
+		
+		System.out.println("calc--->>"+tmp);
+		
+		System.out.println("=================================");
+		
+		CalculaterJava7 calcJava7 = CalculaterJava7.CALC_B;
+		long tmp2 = calcJava7.calculater(50);
+		System.out.println("calc--->>"+tmp2);
+		
 	}
 }
